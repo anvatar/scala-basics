dfcbbd68395c2eb02a84989fec4c0772ac985680
안홍준 <anvatar@daumcorp.com>
2014-08-11 22:09:41 +0900

    1장 연습문제 풀이 템플릿 추가

---
+++ src/test/scala/impatient/chapter01/ChapterSpec.scala
@@ -0,0 +1,65 @@
+package impatient.chapter01
+
+import org.scalatest._
+
+class ChapterSpec extends FlatSpec with Matchers {
+  //
+  // 연습문제 1
+  //
+
+  /*
+          scala> 3.
+          %   +   >    >>>            isInstanceOf   toDouble   toLong     unary_+   |
+          &   -   >=   ^              toByte         toFloat    toShort    unary_-
+          *   /   >>   asInstanceOf   toChar         toInt      toString   unary_~
+   */
+
+
+  //
+  // 연습문제 2
+  //
+
+
+  //
+  // 연습문제 3
+  //
+
+
+  //
+  // 연습문제 4
+  //
+
+
+  //
+  // 연습문제 5
+  //
+
+
+  //
+  // 연습문제 6
+  //
+
+  "2 ^ 1024" should "be " + BigInt(2).pow(1024) in {
+    true should be === true
+  }
+
+  //
+  // 연습문제 7
+  //
+
+
+  //
+  // 연습문제 8
+  //
+
+
+  //
+  // 연습문제 9
+  //
+
+
+  //
+  // 연습문제 10
+  //
+
+}
