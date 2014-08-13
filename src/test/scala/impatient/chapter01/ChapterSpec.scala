48ca02a7773137c9541fe7faa55b6a292766a79e
안홍준 <anvatar@daumcorp.com>
2014-08-13 10:16:01 +0900

    좀 더 의미 있는 파라미터 값 사용

--- src/test/scala/impatient/chapter01/ChapterSpec.scala
+++ src/test/scala/impatient/chapter01/ChapterSpec.scala
@@ -124,7 +124,7 @@ class ChapterSpec extends FlatSpec with Matchers {
         - beginIndex나 endIndex를 잘못 계산하면 StringIndexOutOfBoundsException이 발생하기 쉽다.
 
       take, drop, takeRight, dropRight를 사용하는 것이 의미가 더 명확하며, 대부분의 경우 코드도 더 간결해진다.
-      단, 중간에서 일정 길이의 문자열을 취해야 할 때에는 substring을 사용한 코드가 더 간결하다.
+      단, 중간에서 일정 길이의 문자열을 취해야 할 때에는 substring을 사용한 코드가 더 명확하고 간결하다.
    */
 
   "take, drop, takeRight, dropRight" should "be better than substring" in {
@@ -142,10 +142,10 @@ class ChapterSpec extends FlatSpec with Matchers {
     str dropRight 5 shouldEqual "Scala for the Impa"
     str.substring(0, str.length - 5) shouldEqual "Scala for the Impa"
 
-    str drop 5 dropRight 5 shouldEqual " for the Impa"
-    str.substring(5, str.length - 5) shouldEqual " for the Impa"
+    str drop 6 dropRight 4 shouldEqual "for the Impat"
+    str.substring(6, str.length - 4) shouldEqual "for the Impat"
 
-    str take 10 takeRight 5 shouldEqual " for "
-    str.substring(5, 10) shouldEqual " for "
+    str take 10 takeRight 6 shouldEqual "a for "
+    str.substring(4, 10) shouldEqual "a for "
   }
 }
