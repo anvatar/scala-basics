92eb31e3f79ad5060c23684f85c8ad11ab7acf2a
HongJoon Ahn <anvatar@gmail.com>
2014-09-02 22:35:37 +0900

    연습문제 9-2

--- src/test/scala/impatient/chapter09/ChapterSuite.scala
+++ src/test/scala/impatient/chapter09/ChapterSuite.scala
@@ -1,5 +1,6 @@
 package impatient.chapter09
 
+import java.io.PrintWriter
 import java.net.URI
 
 import org.scalatest.FunSuite
@@ -46,6 +47,49 @@ class ChapterSuite extends FunSuite {
   // 연습문제 9-2
   //
 
+  def convertTabsToSpaces(filePath: String, tabWidth: Int): Unit = {
+    def convert(str: String): String = {
+      val builder = new StringBuilder
+      for (c <- str) {
+        builder ++= (if (c == '\t') " " * ((builder.length / tabWidth + 1) * tabWidth - builder.length) else c.toString)
+      }
+      builder.mkString
+    }
+
+    val source = Source.fromFile(filePath)
+    val lines = source.getLines().toArray.map(convert)
+    source.close()
+
+    val out = new PrintWriter(filePath)
+    for (l <- lines) out.println(l)
+    out.close()
+  }
+
+  test("convertTabsToSpaces") {
+    def prepareTestFile(testResourcePath: String, testFilePath: String) {
+      val source = Source.fromURI(ClassLoader.getSystemClassLoader.getResource(testResourcePath).toURI)
+      val lines = source.getLines().toArray
+      source.close()
+
+      val out = new PrintWriter(testFilePath)
+      for (l <- lines) out.println(l)
+      out.close()
+    }
+
+    // 홀수 줄은 탭으로 구분된 라인, 짝수 줄은 같은 내용인데 공백문자로 구분된 라인으로 구성된 텍스트 파일
+    val testFilePath = "/tmp/with-tabs-then-spaces.txt"
+
+    prepareTestFile("impatient/with-tabs-then-spaces.txt", testFilePath)
+
+    convertTabsToSpaces(testFilePath, 4)
+
+    val source = Source.fromFile(testFilePath)
+    val lines = source.getLines().toArray
+    source.close()
+
+    for (i <- (1 to lines.length) filter (_ % 2 == 1))
+      assert(lines(i - 1) == lines(i), "line " + (i - 1) + " ~ " + i)
+  }
 
   //
   // 연습문제 9-3
