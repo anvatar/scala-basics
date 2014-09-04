ce6f1650de2afa5b9a595ed6a8bd61631195b44e
황영주 <yeonju1213@gmail.com>
2014-09-04 18:43:05 +0900

    chapter07

--- src/main/scala/impatient/chapter07/random/package.scala
+++ src/main/scala/impatient/chapter07/random/package.scala
@@ -1,8 +1,22 @@
 package impatient.chapter07
 
-/**
- * Created by siena on 2014. 9. 4..
- */
 package object random {
 
+  val a = 1664525
+  val b = 1013904223
+  val n = 32
+  var previousIntVal:Int = 0
+  var previousDoubleVal:Double = 0
+
+  def nextInt() = {
+    previousIntVal= (previousIntVal * a + b) % math.pow(2, n).toInt
+    previousIntVal
+  }
+
+  def nextDouble() = {
+    previousDoubleVal = (previousDoubleVal * a + b) % math.pow(2, n)
+    previousDoubleVal
+  }
+
+  def setSeed(n:Int ) { previousIntVal = n; previousDoubleVal = n.toDouble }
 }
--- src/test/scala/impatient/chapter07/ChapterSpec.scala
+++ src/test/scala/impatient/chapter07/ChapterSpec.scala
@@ -11,11 +11,16 @@ class ChapterSpec extends FlatSpec with Matchers {
   // 연습문제 2
   //
 
-
   //
   // 연습문제 3
   //
-
+  "RandomPackageObjectTest" should "be" in {
+    random.setSeed(1)
+    1015568748 shouldEqual random.nextInt()
+    1586005467 shouldEqual random.nextInt()
+    1015568748.0 shouldEqual random.nextDouble()
+    1586005467.0 shouldEqual random.nextDouble()
+  }
 
   //
   // 연습문제 4
@@ -30,50 +35,20 @@ class ChapterSpec extends FlatSpec with Matchers {
   //
   // 연습문제 6
   //
-  "Q6" should "be" + {var x:Long = 1L; for (e <- "Hello") x *= e.toLong; x} in {
-    9415087488L shouldEqual {var x:Long = 1L; for (e <- "Hello") x *= e.toLong; x}
-  }
 
   //
   // 연습문제 7
   //
-  "Q7" should "be" + {var x:Long=1L; "Hello".foreach(x *= _); x} in {
-    9415087488L shouldEqual {var x:Long=1L; "Hello".foreach(x *= _); x}
-  }
-
 
   //
   // 연습문제 8
   //
-  /*
-          scala> def product(s: String) = {
-               |   var x:Long = 1L
-               |   s.foreach(x*=_)
-               |   x
-               | }
-          product: (s: String)Long
-
-          scala> product("Hello")
-          res0: Long = 9415087488
-   */
 
   //
   // 연습문제 9
   //
-  /*
-          scala> def product(s: String):Long = {
-               |   if (s.length == 1) s.charAt(0).toLong else s.charAt(0).toLong * product(s.drop(1))
-               | }
-          product: (s: String)Long
-
-          scala> product("Hello")
-          res1: Long = 9415087488
-   */
 
   //
   // 연습문제 10
   //
-  /*
-  
-   */
 }
