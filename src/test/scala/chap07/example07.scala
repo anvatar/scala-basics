6d71613536cec3bf1a1696727952d8bf86b88aca
kwonyoungjoo <justdoit1978@daum.net>
2014-09-04 16:25:44 +0900

    chap07 연습 문제 풀이

--- src/test/scala/chap07/example07.scala
+++ src/test/scala/chap07/example07.scala
@@ -4,6 +4,40 @@ package chap07
  * Created by kwonyoungjoo on 14. 9. 1..
  * example 7-3
  */
-class example07 {
 
-}
+
+  /*
+    연습문제 7-2
+   */
+
+
+
+   package  object randomLinear {
+     var seed: Int = 0
+     val a: Int = 1664525
+     val b: Int = 1013904223
+     val n: Int = 32
+
+     def next(): Double = {
+       seed * a + (b % 2)
+     }
+
+     def nextInt(): Int = {
+       next().toInt
+     }
+
+     def nextDouble(): Double = {
+       next()
+     }
+
+     def setSeed(seed: Int) {
+       this.seed = seed
+     }
+   }
+
+  class randomTest {
+    randomLinear.setSeed(10)
+    println(randomLinear.nextDouble())
+  }
+
+
