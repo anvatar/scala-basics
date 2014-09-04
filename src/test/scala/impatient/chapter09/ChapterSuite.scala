70d65064c4441e836eb5e5896e773f7f2626f3e6
HongJoon Ahn <anvatar@gmail.com>
2014-09-05 00:31:25 +0900

    연습문제 9-4

--- src/test/scala/impatient/chapter09/ChapterSuite.scala
+++ src/test/scala/impatient/chapter09/ChapterSuite.scala
@@ -106,6 +106,70 @@ class ChapterSuite extends FunSuite {
   // 연습문제 9-4
   //
 
+  /*
+    impatient.chapter07.random 패키지를 이용하여 다음 코드로 floating-point-numbers.txt 파일을 준비했다.
+
+        for (i <- 1 to 50) {
+          val sign = if (nextInt() % 2 == 0) 1 else -1
+          val exp = nextInt() % 16 + 8
+          val mantissa = nextDouble() * 10
+          val str = (if (sign > 0) "" else "-") + mantissa.toString.dropRight(3) + "E" + exp.toString
+          println(str.toDouble)
+        }
+   */
+
+  class DoubleStat {
+    private var _sum: Double = 0
+    private var _count: Int = 0
+    private var _max: Double = 0
+    private var _min: Double = 0
+
+    def sum = _sum
+
+    def count = _count
+
+    def max = _max
+
+    def min = _min
+
+    def average = if (isEmpty) 0.0 else sum / count
+
+    private def append(d: Double): Unit = {
+      if (isEmpty) {
+        _max = d
+        _min = d
+      }
+
+      _max = math.max(_max, d)
+      _min = math.min(_min, d)
+      _sum += d
+      _count += 1
+    }
+
+    private def isEmpty = _count <= 0
+  }
+
+  object DoubleStat {
+    def fromResource(resourcePath: String): DoubleStat = {
+      val doubleStat = new DoubleStat
+
+      val source = Source.fromURI(resourceUri(resourcePath))
+      for (l <- source.getLines()) doubleStat.append(l.toDouble)
+      source.close()
+
+      doubleStat
+    }
+  }
+
+  test("DoubleStat") {
+    val doubleStat = DoubleStat.fromResource("impatient/floating-point-numbers.txt")
+
+
+    assertResult(1.180796815842919E+24)(doubleStat.sum)
+    assertResult(2.361593631685838E+22)(doubleStat.average)
+    assertResult(9.36467935083E+23)(doubleStat.max)
+    assertResult(-5.851349620113E+22)(doubleStat.min)
+  }
 
   //
   // 연습문제 9-5
