7b9e0d2f5a251f3d5d79c8c7a7b33c6a2630ce73
HongJoon Ahn <anvatar@gmail.com>
2014-09-15 23:32:41 +0900

    연습문제 11-1

--- src/test/scala/impatient/chapter11/ChapterSuite.scala
+++ src/test/scala/impatient/chapter11/ChapterSuite.scala
@@ -8,6 +8,23 @@ class ChapterSuite extends FunSuite {
   // 연습문제 11-1
   //
 
+  /*
+      `+` 연산자와 `->` 연산자의 우선순위는 같고, 둘 다 왼쪽으로 결합하는(left associative) 일반적인 경우에 해당한다.
+
+      3 + 4 -> 5 ==> (3 + 4) -> 5
+
+          scala> 3 + 4 -> 5
+          res0: (Int, Int) = (7,5)
+
+      3 -> 4 + 5 ==> (3 -> 4) + 5
+
+          scala> 3 -> 4 + 5
+          <console>:8: error: type mismatch;
+           found   : Int(5)
+           required: String
+                        3 -> 4 + 5
+                                 ^
+   */
 
   //
   // 연습문제 11-2
