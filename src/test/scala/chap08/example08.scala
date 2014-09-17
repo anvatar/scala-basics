4b2080c12f56f16ebafdf68e85b89e6570f49ffa
kwonyoungjoo <justdoit1978@daum.net>
2014-09-17 13:12:31 +0900

    연습문제 08풀이

---
+++ src/test/scala/chap08/example08.scala
@@ -0,0 +1,53 @@
+package chap08
+
+import org.scalatest.FunSuite
+
+/**
+ * Created by kwonyoungjoo on 14. 9. 17..
+ */
+class example08 extends FunSuite{
+
+  abstract class Item{
+    def price:Double
+    def description:String
+  }
+
+  class SimpleItem(val price:Double, val description:String) extends Item
+
+  class Bundle {
+    private var items = Array[Item]()
+
+    def addItem(item:Item){
+      this.items :+ item
+    }
+
+    def price = {
+      var sumPrice:Double = 0 ;
+      println(items.length)
+      for(item <- items) { sumPrice += item.price }
+
+      sumPrice
+    }
+
+    def description = {
+       var bundleDescription:String = ""
+       for(item <- this.items) bundleDescription += item.description
+    }
+
+  }
+
+  test("extends test"){
+    val firstItem = new SimpleItem(100.0, "first Item")
+    val secondItem = new SimpleItem(200.0, "second Item")
+    val thirdItem = new SimpleItem(300.0, "third Item")
+
+    val bundle = new Bundle
+    bundle.addItem(firstItem)
+    bundle.addItem(secondItem)
+    bundle.addItem(thirdItem)
+
+    assertResult(600.0)(bundle.price)
+    println( bundle.description)
+  }
+
+}
