96c56445ac977715ae4008aa76631394e4f2cfa8
HongJoon Ahn <anvatar@gmail.com>
2014-09-19 08:42:59 +0900

    연습문제 11-3

--- src/test/scala/impatient/chapter11/ChapterSuite.scala
+++ src/test/scala/impatient/chapter11/ChapterSuite.scala
@@ -2,6 +2,8 @@ package impatient.chapter11
 
 import org.scalatest.FunSuite
 
+import scala.math.abs
+
 class ChapterSuite extends FunSuite {
 
   //
@@ -39,6 +41,63 @@ class ChapterSuite extends FunSuite {
   // 연습문제 11-3
   //
 
+  class Fraction(n: Int, d: Int) {
+    /*
+     * 책에서 제공
+     */
+
+    private val num: Int = if (d == 0) 1 else n * sign(d) / gcd(n, d)
+    private val den: Int = if (d == 0) 0 else d * sign(d) / gcd(n, d)
+
+    override def toString = num + "/" + den
+
+    def sign(a: Int) = if (a > 0) 1 else if (a < 0) -1 else 0
+
+    def gcd(a: Int, b: Int): Int = if (b == 0) abs(a) else gcd(b, a % b)
+
+    /*
+     * IntelliJ IDEA로 생성
+     */
+
+    def canEqual(other: Any): Boolean = other.isInstanceOf[Fraction]
+
+    override def equals(other: Any): Boolean = other match {
+      case that: Fraction =>
+        (that canEqual this) &&
+          num == that.num &&
+          den == that.den
+      case _ => false
+    }
+
+    override def hashCode(): Int = {
+      val state = Seq(num, den)
+      state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+    }
+
+    /*
+     * 직접 구현
+     */
+
+    def +(other: Fraction): Fraction = Fraction(this.num * other.den + other.num * this.den, this.den * other.den)
+
+    def -(other: Fraction): Fraction = this + Fraction(-1 * other.num, other.den)
+
+    def *(other: Fraction): Fraction = Fraction(this.num * other.num, this.den * other.den)
+
+    def /(other: Fraction): Fraction = this * Fraction(other.den, other.num)
+
+  }
+
+  object Fraction {
+    def apply(n: Int, d: Int) = new Fraction(n, d)
+  }
+
+  test("Fraction") {
+    assertResult(Fraction(7, 6))(Fraction(1, 2) + Fraction(2, 3))
+    assertResult(Fraction(-5, 12))(Fraction(1, 3) - Fraction(3, 4))
+    assertResult(Fraction(-7, 8))(Fraction(7, 6) * Fraction(3, -4))
+    assertResult(Fraction(7, 4))(Fraction(7, 6) / Fraction(2, 3))
+  }
 
   //
   // 연습문제 11-4
