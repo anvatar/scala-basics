5d7de7ce590202555efb89240e58e3acea9345d4
mikai21 <mikai21@gmail.com>
2014-09-22 23:00:38 +0900

    new chapter10

---
+++ src/test/scala/chapter10/Chapter10.scala
@@ -0,0 +1,62 @@
+package chapter10
+
+import org.scalatest.{FlatSpec, Matchers}
+
+/**
+ * Created by parkhoyong on 2014. 8. 21..
+ */
+class Chapter10 extends FlatSpec with Matchers {
+  //
+  // 연습문제 1
+  //
+
+  /*
+          scala> 3.
+          %   +   >    >>>            isInstanceOf   toDouble   toLong     unary_+   |
+          &   -   >=   ^              toByte         toFloat    toShort    unary_-
+          *   /   >>   asInstanceOf   toChar         toInt      toString   unary_~
+   */
+
+
+  //
+  // 연습문제 2
+  //
+
+  //
+  // 연습문제 3
+  //
+
+  //
+  // 연습문제 4
+  //
+
+
+  //
+  // 연습문제 5
+  //
+
+
+  //
+  // 연습문제 6
+  //
+
+  //
+  // 연습문제 7
+  //
+
+
+  //
+  // 연습문제 8
+  //
+
+
+  //
+  // 연습문제 9
+  //
+
+
+  //
+  // 연습문제 10
+  //
+
+}
