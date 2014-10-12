5abae11f304b4d2b20b7b522626026e806633e72
HongJoon Ahn <anvatar@gmail.com>
2014-10-12 22:48:16 +0900

    연습문제 14-2 ~ 14-3

--- src/test/scala/impatient/chapter14/ChapterSuite.scala
+++ src/test/scala/impatient/chapter14/ChapterSuite.scala
@@ -8,16 +8,37 @@ class ChapterSuite extends FunSuite {
   // 연습문제 14-1
   //
 
+  /*
+    TODO: 일단 생략
+   */
 
   //
   // 연습문제 14-2
   //
 
+  def tupleSwap(pair: (Int, Int)): (Int, Int) = pair match {
+    case (a, b) => (b, a)
+  }
+
+  test("tupleSwap") {
+    assertResult((1, 2))(tupleSwap((2, 1)))
+  }
 
   //
   // 연습문제 14-3
   //
 
+  val arraySwap: PartialFunction[Array[Int], Array[Int]] = {
+    case Array(a, b, rest@_*) => Array(b, a) ++ rest
+  }
+
+  test("arraySwap") {
+    assertResult(Array(1, 2))(arraySwap(Array(2, 1)))
+    assertResult(Array(1, 2, 3))(arraySwap(Array(2, 1, 3)))
+    intercept[MatchError] {
+      arraySwap(Array(1))
+    }
+  }
 
   //
   // 연습문제 14-4
