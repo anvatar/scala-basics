3ce23a048de253e51333861f5603ab0044c2d7b0
HongJoon Ahn <anvatar@gmail.com>
2014-10-12 22:48:56 +0900

    연습문제 14-4

--- src/test/scala/impatient/chapter14/ChapterSuite.scala
+++ src/test/scala/impatient/chapter14/ChapterSuite.scala
@@ -44,6 +44,36 @@ class ChapterSuite extends FunSuite {
   // 연습문제 14-4
   //
 
+  sealed abstract class Item
+
+  case class Article(description: String, price: Double) extends Item
+
+  case class Bundle(description: String, discount: Double, items: Item*) extends Item
+
+  case class Multiple(amount: Int, item: Item) extends Item
+
+  def price(it: Item): Double = it match {
+    case Article(_, p) => p
+    case Bundle(_, discount, its@_*) => its.map(price).sum - discount
+    case Multiple(amount, item) => amount * price(item)
+  }
+
+  test("Item") {
+    val bundle = Bundle("Father's day special", 20.0,
+      Article("Scala for the Impatient", 39.95),
+      Bundle("Anchor Distillery Sampler", 10.0,
+        Article("Old Potrero Straight Rye Whiskey", 79.95),
+        Article("Junîpero Gin", 32.95)))
+    assert(price(bundle) == 39.95 + (79.95 + 32.95 - 10.0) - 20.0)
+
+    assert(price(Multiple(7,
+      Article("Scala for the Impatient", 39.95))) == 7 * 39.95)
+
+    assert(price(Multiple(3,
+      Bundle("Anchor Distillery Sampler", 10.0,
+        Article("Old Potrero Straight Rye Whiskey", 79.95),
+        Article("Junîpero Gin", 32.95)))) == 3 * (79.95 + 32.95 - 10.0))
+  }
 
   //
   // 연습문제 14-5
