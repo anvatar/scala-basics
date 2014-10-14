52dcea871824c3ef82ddbba2fa59317c1f3173d8
HongJoon Ahn <anvatar@gmail.com>
2014-10-14 20:51:34 +0900

    연습문제 15-5

--- src/main/scala/impatient/chapter15/ScalaUtil.scala
+++ src/main/scala/impatient/chapter15/ScalaUtil.scala
@@ -1,7 +1,12 @@
 package impatient.chapter15
 
+import java.io.IOException
+
 import scala.annotation.varargs
+import scala.io.Source
 
 object ScalaUtil {
   @varargs def sum(values: Int*) = values.sum
+
+  @throws(classOf[IOException]) def readAll(filePath: String) = Source.fromFile(filePath).mkString
 }
--- src/test/java/impatient/chapter15/JavaSuite.java
+++ src/test/java/impatient/chapter15/JavaSuite.java
@@ -2,6 +2,8 @@ package impatient.chapter15;
 
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.junit.Assert.assertEquals;
 
 public class JavaSuite {
@@ -15,4 +17,26 @@ public class JavaSuite {
 		assertEquals(15, ScalaUtil.sum(1, 2, 3, 4, 5));
 	}
 
+	//
+	// 연습문제 15-5
+	//
+
+	@Test
+	public void readAll() {
+		/*
+			ScalaUtil.readAll()이 다음과 같이 선언되어 있으면
+
+				def readAll(filePath: String) = ???
+
+			다음과 같이 작성해도 IOException을 catch 하라는 컴파일 에러가 발생하지 않는다.
+
+				ScalaUtil.readAll("/tmp/non-existing/filepath");
+		 */
+
+		try {
+			ScalaUtil.readAll("/tmp/non-existing/filepath");
+		} catch (IOException e) {	/* IOException을 catch 하지 않으면 컴파일 에러가 발생한다. */
+			// do nothing
+		}
+	}
 }
